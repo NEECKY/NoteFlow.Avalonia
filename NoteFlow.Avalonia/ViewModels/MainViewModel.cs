68c1c316b5bd3548f331ff01cbf6083e957938c1
Nikita Barsegyan <89491657+NEECKY@users.noreply.github.com>
2025-06-08 17:31:14 +0300

    Init

---
+++ NoteFlow.Avalonia/ViewModels/MainViewModel.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace NoteFlow.Avalonia.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
+{
+    [ObservableProperty] private string _greeting = "Welcome to Avalonia!";
+}
---
+++ NoteFlow.Avalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace NoteFlow.Avalonia.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
